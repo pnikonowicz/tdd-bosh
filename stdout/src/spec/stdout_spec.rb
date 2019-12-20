7e080942a53614a8887ed27acbfb9764ef7425e6
pnikonowicz <paul.nikonowicz@gmail.com>
2019-12-20 10:45:14 -0500

    fix test from previous refactorings

--- stdout/src/spec/stdout_spec.rb
+++ stdout/src/spec/stdout_spec.rb
@@ -1,11 +1,11 @@
 require 'rspec'
 require 'json'
-require 'yaml' # todo fix bosh-template
+require 'yaml'
 require 'bosh/template/test'
 
 describe 'stdout' do
     let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../..')) }
-    let(:job) { release.job('stdout') }
+    let(:job) { release.job('main') }
     let(:template) { job.template('main.sh') }
     
     it 'writes to property to stdout' do
